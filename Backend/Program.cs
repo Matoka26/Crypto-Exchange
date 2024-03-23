ac579fc1f7e7d2749c517e8ee86d00bef28fb137
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2024-03-23 10:18:54 +0200

    added seeders

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using test_binance_api.Data;
 using test_binance_api.Helpers.Extensions;
+using test_binance_api.Helpers.Seeders;
 using test_binance_api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,9 +30,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddRepositories();
 builder.Services.AddServices();
+builder.Services.AddSeeder();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
+SeedData(app);
 
 if (app.Environment.IsDevelopment())
 {
@@ -46,3 +49,20 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var userService = scope.ServiceProvider.GetService<UserSeeder>();
+        userService.SeedInitialUsers();
+
+        var roleService = scope.ServiceProvider.GetService<RoleSeeder>();
+        roleService.SeedInitialRoles();
+
+        var userRoleService = scope.ServiceProvider.GetService<UserRoleSeeder>();
+        userRoleService.SeedInitialUserRole();
+
+    }
+}
